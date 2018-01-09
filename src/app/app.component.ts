b3e50a023effac12df08031573c56d7baacecc4c
daton <rapidclimate@gmail.com>
2018-01-09 14:54:03 -0600

    Ya con  consultaoii

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
+  onBasicUpload(event) {
+    //event.files == files to upload
+    console.log("Se subio");
+   // console.log("ESto llego "+JSON.stringify(event));
+   //El siguiente si nos da la respuesta basado en la consulta realziada
+   console.log(JSON.parse(event.xhr.response));
+   console.log("Respuesta como string "+JSON.stringify(event.xhr.response))
+}
+
 }
